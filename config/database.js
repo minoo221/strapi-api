664abed86652b23349d035f2f635b001f24457c7
Milan Blasko <milan.blasko@interway.sk>
2021-02-04 17:19:28 +0100

    add strapi

--- config/database.js
+++ config/database.js
@@ -1,15 +1,36 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
+const parse = require('pg-connection-string').parse
+
+module.exports = ({ env }) => {
+  if(env('NODE_ENV') === 'production') {
+
+  }
+  /* return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'sqlite',
+            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
     },
-  },
-});
+  } */
+  return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'mongoose',
+          settings: {
+            uri: env('DATABASE_URI'),
+          },
+          options: {
+            ssl: true,
+          },
+        },
+    },
+  }
+};
